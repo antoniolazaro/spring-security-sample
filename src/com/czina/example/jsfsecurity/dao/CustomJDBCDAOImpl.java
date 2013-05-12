f9ceada186ce98b0aaccad7ac4e401c95a64c6c3
Antonio Lazaro Carvalho Borges <antonio.lazaro@gmail.com>
2013-05-12 18:26:31 -0300

    Conexão com tabela de usuario especifica. Para não usar o modelo default do spring security, para sistemas que desejam acoplar o security após o sistema já estar feito.

---
+++ src/com/czina/example/jsfsecurity/dao/CustomJDBCDAOImpl.java
@@ -0,0 +1,10 @@
+package com.czina.example.jsfsecurity.dao;
+
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+
+public class CustomJDBCDAOImpl extends JdbcDaoImpl {
+
+	 public CustomJDBCDAOImpl(){
+	        super();
+	    }
+}
